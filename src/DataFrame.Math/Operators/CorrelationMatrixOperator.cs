2960b16feab8b64a77823e6e09d5a147a8b11559
Richard Conway <richard@elastacloud.com>
2017-10-02 13:38:28 +0100

    added correlation and covariance matrices again

---
+++ src/DataFrame.Math/Operators/CorrelationMatrixOperator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using DataFrame.Math.Data;
+
+namespace DataFrame.Math.Operators
+{
+    public static class CorrelationMatrixOperator
+    {
+       public static Matrix<double> CorrelationMatrix(this Matrix<double> source)
+       {
+          var sd = new List<double>();
+          for (int i = 0; i < source.ColumnCount; i++)
+          {
+             sd.Add(new ReadOnlyCollection<double>(source.GetColumn(i)).StandardDeviation());
+          }
+
+          var matrix = new Matrix<double>(source.ColumnCount, source.RowCount);
+          for (int i = 0; i < source.ColumnCount; i++)
+          {
+             for (int j = 0; j < source.RowCount; j++)
+             {
+                matrix[i, j] = source[i, j] / (sd[i] * sd[j]);
+
+             }
+          }
+
+          return matrix;
+      }
+
+
+      
+   }
+}
---
+++ src/DataFrame.Math/Operators/CovarianceMatrixOperator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using DataFrame.Math.Data;
+
+namespace DataFrame.Math.Operators
+{
+    public static class CovarianceMatrixOperator
+    {
+       public static Matrix<double> CovarianceMatrix(this Matrix<double> source)
+       {
+          List<double> means = new List<double>(), sums = new List<double>();
+
+          var columnDetails = new List<IList>();
+
+          for (int i = 0; i < source.ColumnCount; i++)
+          {
+             var col = new ReadOnlyCollection<double>(source.GetColumn(i));
+             means.Add(col.Mean());
+             sums.Add(col.Sum());
+             columnDetails.Add(col);
+          }
+          // Cov(x,y) = E{xy} - E{x}E{y}. 
+          double covariance = 0.0D;
+          var matrix = new Matrix<double>(source.ColumnCount, source.RowCount);
+          for (int rows = 0; rows < source.ColumnCount; rows++)
+          {
+             for (int cols = 0; cols < source.ColumnCount; cols++)
+             {
+                // loop in here against the underlying column values
+                for (int i = 0; i < source.RowCount; i++)
+                {
+                   covariance += ((double) columnDetails[rows][i] - means[rows]) *
+                                 ((double) columnDetails[cols][i] - means[cols]);
+                }
+
+                matrix[rows, cols] = covariance / (double) (source.RowCount - 1);
+                covariance = 0.0D;
+             }
+          }
+
+          return matrix;
+       }
+    }
+}
